c8c01fadd46109b3c99f72f7dcea3f4a24b19f9f
MAVOTUNA <sdm03131229@gmail.com>
2022-06-27 15:45:33 +0900

    테스트1

--- src/main/java/com/dailylife/global/swagger/SwaggerConfig.java
+++ src/main/java/com/dailylife/global/swagger/SwaggerConfig.java
@@ -19,7 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
     /**
-     * http://localhost:8080/swagger-ui.html#ddddddddddddddddddddd
+     * http://localhost:8080/swagger-ui.html#dddddddddddddddddddddddd
      */
 
     @Bean
