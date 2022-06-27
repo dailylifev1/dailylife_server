e63ca802282bd482b1115aa1766cfbad91c00c8b
Yang GYEONGPIL <ykp9711@gmail.com>
2022-06-27 21:01:36 +0900

    깃 테스트

--- src/main/java/com/dailylife/global/swagger/SwaggerConfig.java
+++ src/main/java/com/dailylife/global/swagger/SwaggerConfig.java
@@ -19,6 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
     /**
+     * 깃 테스트
      * http://localhost:8080/swagger-ui.html#dddddddddddddddddddddddd123dddddddddd
      */
     @Bean
