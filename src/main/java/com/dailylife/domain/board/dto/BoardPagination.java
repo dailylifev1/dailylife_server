8c31f32c1addc16afba3846526e66b1dbfdcd712
ygp <ykp9711@gmail.com>
2022-07-30 15:33:09 +0900

    게시글 페이징, 회원가입 시 중복된 iD로 가입 접근 시 에러메세지 출력 #74 DAILYLIFE2-24
    1. 게시글을 페이지 당 15개씩 전송
    
    2. 이미 가입된 ID로 가입 할 경우 에러메세지 출력

--- src/main/java/com/dailylife/domain/board/dto/BoardPagination.java
+++ src/main/java/com/dailylife/domain/board/dto/BoardPagination.java
@@ -7,6 +7,11 @@ public class BoardPagination {
         int pg = 1;        // 현재 페이지
         int sz = 15;       // 페이지 당 레코드 수
 
+        int recordCount;   // 전체 레코드 수
+
+        public String getQueryString() {
+            return String.format("pg=%d&sz=%d", pg, sz);
+        }
     }
 
 
